e0052f8765234507491d04b29e93bea469b5c094
kim-dongho <dongho_k@naver.com>
2021-09-23 00:58:39 +0900

    first cover-letter

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="introduce">
+        <span className="introduce__name">
+          안녕하세요<br/>
+          신입 프론트엔드 개발자<br/>
+          김동호입니다.
+        </span>
+      </div>
+      <div className="project">
+        <span>Project</span>
+        <div className="project__first">
+
+        </div>
+        <div className="project__second">
+
+        </div>
+      </div>
+      <div className="skill">
+        <span>Skills</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
